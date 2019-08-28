d7230a8bccf0e2be3bed4f81323aae10e6ef0380
CaioVitor <caio.gobira@sunn-it.com>
2019-08-27 22:04:01 -0300

    Criado um controller para a API REST que responde a mensagens para verificar o funcionamento

---
+++ src/main/java/REST/Controllers/ServerAwakeMessageController.java
@@ -0,0 +1,30 @@
+package REST.Controllers;
+
+import REST.Application;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@Controller
+public class ServerAwakeMessageController {
+
+    @GetMapping("/api/v1/")
+    public ResponseEntity<String> awakeTime() {
+        return ResponseEntity
+                .ok().header("Content-Type"," text/html; charset=utf-8" )
+                .body("Hello! I'm an awake server. I've been working since " + Application.awakeDate);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity
+                .ok().header("Content-Type"," text/html; charset=utf-8" )
+                .body("Hello!");
+    }
+
+}
