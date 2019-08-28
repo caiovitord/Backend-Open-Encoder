5a959454380a1bccdc7d3a3742c56679db663d7d
CaioVitor <caio.gobira@sunn-it.com>
2019-08-27 22:06:44 -0300

    Alterações para que o repository possa ser testado

--- src/main/java/REST/Repositories/VideoEncodingRequestRepository.java
+++ src/main/java/REST/Repositories/VideoEncodingRequestRepository.java
@@ -4,6 +4,7 @@ package REST.Repositories;
 import Persistence.DAO.VideoEncodingRequestDAO;
 import Persistence.DataSourceSingleton;
 import Persistence.Entities.VideoEncodingRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
@@ -13,23 +14,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-@Component
 public class VideoEncodingRequestRepository implements JpaRepository<VideoEncodingRequest, String> {
 
-
     VideoEncodingRequestDAO videoEncodingRequestDAO = new VideoEncodingRequestDAO(DataSourceSingleton.getInstance().getEntityManager());
+
+    public VideoEncodingRequestRepository() {
+    }
 
     @Override
     public Optional<VideoEncodingRequest> findById(String s) {
         return Optional.ofNullable(videoEncodingRequestDAO.find(s));
     }
 
-
-
+    @Override
+    public <S extends VideoEncodingRequest> S save(S s) {
+        videoEncodingRequestDAO.create(s);
+        return s;
+    }
 
     @Override
     public void delete(VideoEncodingRequest request) {
@@ -42,6 +49,10 @@ public class VideoEncodingRequestRepository implements JpaRepository<VideoEncodi
     }
 
 
+    /**Construtor somente utilizado em testes*/
+    public VideoEncodingRequestRepository(EntityManager test_persistence_unit) {
+        videoEncodingRequestDAO = new VideoEncodingRequestDAO(test_persistence_unit);
+    }
     /**
      *
      * Operações não implementadas pois não serão usadas na API REST
@@ -58,10 +69,7 @@ public class VideoEncodingRequestRepository implements JpaRepository<VideoEncodi
         throw new UnsupportedOperationException();
     }
 
-    @Override
-    public <S extends VideoEncodingRequest> S save(S s) {
-        throw new UnsupportedOperationException();
-    }
+
 
     @Override
     public boolean existsById(String s) {
@@ -83,9 +91,6 @@ public class VideoEncodingRequestRepository implements JpaRepository<VideoEncodi
     public void deleteAll() {
         throw new UnsupportedOperationException();
     }
-
-
-
 
 
 
