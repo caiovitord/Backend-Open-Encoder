7192d812e257c5f9eae3253177c67128ca4ee291
CaioVitor <caio.gobira@sunn-it.com>
2019-08-27 22:03:00 -0300

    Removida algumas classes de Spring Advices que não serão utilizadas

--- src/main/java/REST/Exceptions/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package REST.Exceptions;
-
-public class UserAlreadyExistsException extends RuntimeException {
-
-    public UserAlreadyExistsException(String name) {
-        super("A user with the name: \""+ name+ "\" is already registered ");
-    }
-}
